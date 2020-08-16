bb99b4bd0864879e2edc2993d3d8fce437d8f6ad
wikz87 <wikz@live.com>
2020-08-17 02:29:43 +0500

    fixed babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', "module:react-native-dotenv"],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['babel-plugin-styled-components'],
+    ["module:react-native-dotenv"],
     [
       'module-resolver',
       {
